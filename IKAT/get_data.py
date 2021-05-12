5ecba671441c5830ba0eec7ef9bfedd1e006b3a6
Aadit Trivedi <aadittrivedi@Devangs-iMac-Pro.local>
2021-05-11 20:04:51 -0700

    ikat

--- IKAT/get_data.py
+++ IKAT/get_data.py
@@ -24,18 +24,22 @@ for filename in os.listdir("corpus/"):
             elif "-" in row[0]:
                 prem1 = row[0].split("-")[0]
                 prem2 = row[0].split("-")[1]
-                prem1_num = int(prem1.split("e")[1]) - 1
-                prem2_num = int(prem2.split("e")[1]) - 1
+                prem1_num = int(prem1.split("e")[1])
+                prem2_num = int(prem2.split("e")[1])
                 prem1_str = arg_prems[prem1_num - 1]
                 prem2_str = arg_prems[prem2_num - 1]
-                sources.append(prem1_str + " # " + prem2_str)
                 implicit_prem = row[6]
-                targets.append(prem1_str + " And since " + implicit_prem + " " + prem2_str)
+                if (implicit_prem != ""):
+                    adjacency = row[1]
+                    # if (adjacency == "adjacent"):
+                    sources.append(prem1_str + " # " + prem2_str)
+                    targets.append(prem1_str + " And since " + implicit_prem + " " + prem2_str)
     for source in sources:
         super_sources.append(source)
     for target in targets:
         super_targets.append(target)
 
+print("SOURCES: " + str(len(super_sources)) + " | TARGETS: " + str(len(super_targets)))
 with open('ikat.source', 'w') as f:
     for source in super_sources:
         f.write('%s\n' % source)
