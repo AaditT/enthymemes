3b9b44fbffe8cd670a087b0ba4607582c87bf898
Aadit Trivedi <aadittrivedi@Devangs-iMac-Pro.local>
2021-05-13 00:05:17 -0700

    fixed jandata premises

--- ZeroShot_MaskGen/filter_premise.py
+++ ZeroShot_MaskGen/filter_premise.py
@@ -1,16 +1,20 @@
 prems = []
 counter = 0
 with open('jandata.hypo') as f:
-    for line in f:
+    for line in f.readlines():
         counter += 1
-        if "And since" not in line:
-            prems.append(line)
-            continue
+
+        and_since = False
         
         sents = line.split(".")
         for sent in sents:
             if "And since" in sent:
+                and_since = True
                 prems.append(sent)
+                
+        if and_since == False:
+            prems.append("And since empty")
+            
 
 
 with open('jandata_premises.hypo', 'w') as f:
